753529a67acd309544398bca7fbb7d44931dc97c
KazApps <kazuki_shogi@outlook.jp>
2025-08-01 20:35:53 +0900

    bench: 6831538

--- src/correction.h
+++ src/correction.h
@@ -123,7 +123,7 @@ namespace stormphrax {
             correction += contAdjustment(2, contCorrhist2Weight());
             correction += contAdjustment(4, contCorrhist4Weight());
 
-            score += correction / (2432 - std::min(depth, 48) * 16);
+            score += correction / (2144 - std::min(depth, 48) * 4);
 
             return std::clamp(score, -kScoreWin + 1, kScoreWin - 1);
         }
