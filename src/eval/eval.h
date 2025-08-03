675e983d5ac54d46443601ba1fc1eb22f554e7a1
KazApps <kazuki_shogi@outlook.jp>
2025-08-03 22:36:34 +0900

    bench: 6692985

--- src/eval/eval.h
+++ src/eval/eval.h
@@ -43,7 +43,7 @@ namespace stormphrax::eval {
         i32 eval,
         i32* corrDelta = nullptr
     ) {
-        eval = eval * 512 / (512 + ply + depth);
+        eval = eval * 640 / (640 + ply + depth);
 
         if constexpr (kCorrect) {
             const auto corrected = correction->correct(pos, moves, ply, eval);
