cfe40d19897dede6ac20f7b9bda62183670fe387
KazApps <kazuki_shogi@outlook.jp>
2025-10-31 23:27:32 +0900

    remove killer move bench: 5947759

--- src/search.cpp
+++ src/search.cpp
@@ -376,7 +376,6 @@ namespace stormphrax::search {
         PvList rootPv{};
 
         searchData.nodes = 0;
-        thread.stack[0].killers.clear();
 
         thread.depthCompleted = 0;
 
@@ -846,8 +845,6 @@ namespace stormphrax::search {
             }
         }
 
-        thread.stack[ply + 1].killers.clear();
-
         moveStack.failLowQuiets.clear();
         moveStack.failLowNoisies.clear();
 
@@ -857,7 +854,7 @@ namespace stormphrax::search {
         auto ttFlag = TtFlag::kUpperBound;
 
         auto generator =
-            MoveGenerator::main(pos, moveStack.movegenData, ttMove, curr.killers, thread.history, thread.conthist, ply);
+            MoveGenerator::main(pos, moveStack.movegenData, ttMove, thread.history, thread.conthist, ply);
 
         u32 legalMoves = 0;
 
@@ -1147,8 +1144,6 @@ namespace stormphrax::search {
             const auto penalty = historyPenalty(historyDepth);
 
             if (!pos.isNoisy(bestMove)) {
-                curr.killers.push(bestMove);
-
                 thread.history.updateQuietScore(
                     thread.conthist,
                     ply,
